47ef5d7594af6e8b1315fc94eb157d39538d4dbc
CodeExplorer4267 <rupambhadra478@gmail.com>
2024-11-22 18:03:55 +0530

    commit

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-import http from "http"
-import { percentage } from "./feature.js"
-import fs from "fs"
-const server = http.createServer((req, res) => {
-    //--->First learning----------->
-    // if (req.url === "/percentage") {
-    //     res.end(`
-    //         <html>
-    //             <body>
-    //                 <form id="percentageForm" onsubmit="event.preventDefault(); calculatePercentage();">
-    //                     <input name="name1" placeholder="Enter name1" required />
-    //                     <input name="name2" placeholder="Enter name2" required />
-    //                     <button type="submit">Submit</button>
-    //                 </form>
-    //                 <h2 id="result">Love is: </h2>
+// import http from "http"
+// import { percentage } from "./feature.js"
+// import fs from "fs"
+// const server = http.createServer((req, res) => {
+//     //--->First learning----------->
+//     // if (req.url === "/percentage") {
+//     //     res.end(`
+//     //         <html>
+//     //             <body>
+//     //                 <form id="percentageForm" onsubmit="event.preventDefault(); calculatePercentage();">
+//     //                     <input name="name1" placeholder="Enter name1" required />
+//     //                     <input name="name2" placeholder="Enter name2" required />
+//     //                     <button type="submit">Submit</button>
+//     //                 </form>
+//     //                 <h2 id="result">Love is: </h2>
                     
-    //                 <script>
-    //                     async function calculatePercentage() {
-    //                         const response = await fetch("/getPercentage");
-    //                         const result = await response.text();
-    //                         document.getElementById("result").innerText = "Love is: " + result + "%";
-    //                     }
-    //                 </script>
-    //             </body>
-    //         </html>
-    //     `);
-    // } else if (req.url === "/getPercentage") {
-    //     const result = percentage();
-    //     res.end(result.toString());
-    // } else {
-    //     res.end("404 Not Found");
-    // }
-    //---------------------------------------------------------------------
-   fs.readFile('hello.txt',(err,data)=>{
-      res.end(data)
-   })
-});
-server.listen(5000,()=>{
-    console.log("Server is listening")
-    console.log("http://localhost:5000")
-})
+//     //                 <script>
+//     //                     async function calculatePercentage() {
+//     //                         const response = await fetch("/getPercentage");
+//     //                         const result = await response.text();
+//     //                         document.getElementById("result").innerText = "Love is: " + result + "%";
+//     //                     }
+//     //                 </script>
+//     //             </body>
+//     //         </html>
+//     //     `);
+//     // } else if (req.url === "/getPercentage") {
+//     //     const result = percentage();
+//     //     res.end(result.toString());
+//     // } else {
+//     //     res.end("404 Not Found");
+//     // }
+//     //---------------------------------------------------------------------
+//    fs.readFile('hello.txt',(err,data)=>{
+//       res.end(data)
+//    })
+// });
+// server.listen(5000,()=>{
+//     console.log("Server is listening")
+//     console.log("http://localhost:5000")
+// })
--- server.js
+++ server.js
@@ -2,15 +2,21 @@ import express from "express"
 import path from "path"
 const app=express();
 const PORT=5000;
+app.set("view engine","ejs")
 
 app.get('/',(req,res)=>{
     // res.sendStatus(200)//OK
     // res.sendStatus(403)//Forbidden
     // res.sendStatus(404)//Not Found
     // res.sendStatus(500)//Internal Server Error
+    // res.json({//to send the json data
+    //     success:true,
+    //     products:[]
+    // })
     const pathlocation=path.resolve();
-    res.sendFile(path.join(pathlocation,"./index.html"))
+    res.sendFile(path.join(pathlocation,"index.html"))
 })
+
 app.get('/contact',(req,res)=>{
     res.send("This is Contact page")
 })
