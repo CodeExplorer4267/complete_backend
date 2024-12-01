4ab63c557d1303effcb90f1ac62520d9b3fd72ab
CodeExplorer4267 <rupambhadra478@gmail.com>
2024-12-01 17:55:15 +0530

    commit

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.set("view engine","ejs")
 //     // res.sendFile(path.join(pathlocation,"index.html"))
 // })
 app.use(express.static(path.join(path.resolve(),"public")))//This is how we can serve the static files
+app.use(express.urlencoded({extended:true}))//This middleware extracts the form data
+
+const userdetails=[]
 
 app.get('/',(req,res)=>{
     //this is how we can load an ejs file
@@ -28,14 +31,38 @@ app.get('/',(req,res)=>{
     //     age1:23,
     //     age2:56
     // })
-     res.sendFile("index") //--->We can serve the static files 
-    // res.render("index")//---->We can serve or render the index.ejs file
+    //  res.sendFile("index") //--->We can serve the static files 
+    res.render("index")//---->We can serve or render the index.ejs file
 })
 app.get('/contact',(req,res)=>{
     res.send("This is Contact page")
 })
 app.get('/profile',(req,res)=>{
     res.send("<h1>This is Profile Page</h1>")//er moddheo html diye lekha jabe
+})
+
+app.get("/success",(req,res)=>{
+    res.render("success")
+})
+//---THis is the submit endpoint
+app.post('/submit',(req,res)=>{
+    const data=req.body
+    userdetails.push({
+        username:data.name,
+        email:data.email
+    })
+    // console.log(data)
+    // res.send("<h1> Form submitted successfully")
+    // res.render("success")
+    //---Another method
+    res.redirect("/success")
+})
+
+//---This url endpoint shows the users data
+app.get("/users",(req,res)=>{
+    res.json({
+        userdetails
+    })
 })
 
 app.listen(PORT,()=>{
