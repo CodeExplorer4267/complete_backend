b194ca1260e8ae4eae9bdea19271d09cb33fbb99
CodeExplorer4267 <rupambhadra478@gmail.com>
2024-12-01 16:11:14 +0530

    commit

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const PORT=5000;
 app.set("view engine","ejs")
 
 // app.get('/',(req,res)=>{
+    //----This is about status code-----
 //     // res.sendStatus(200)//OK
 //     // res.sendStatus(403)//Forbidden
 //     // res.sendStatus(404)//Not Found
 //     // res.sendStatus(500)//Internal Server Error
+//    --------------------------------------------
+//    ------This is about json data-----
 //     // res.json({//to send the json data
 //     //     success:true,
 //     //     products:[]
@@ -17,15 +20,16 @@ app.set("view engine","ejs")
 //     // const pathlocation=path.resolve();
 //     // res.sendFile(path.join(pathlocation,"index.html"))
 // })
-app.use(express.static(path.join(path.resolve(),"public")))
+app.use(express.static(path.join(path.resolve(),"public")))//This is how we can serve the static files
+
 app.get('/',(req,res)=>{
     //this is how we can load an ejs file
     // res.render("index",{
     //     age1:23,
     //     age2:56
     // })
-    //  res.sendFile("index")
-    res.render("index")
+     res.sendFile("index") //--->We can serve the static files 
+    // res.render("index")//---->We can serve or render the index.ejs file
 })
 app.get('/contact',(req,res)=>{
     res.send("This is Contact page")
@@ -35,5 +39,5 @@ app.get('/profile',(req,res)=>{
 })
 
 app.listen(PORT,()=>{
-    console.log("Server is listening")
+    console.log("Server is listening ")
 })
